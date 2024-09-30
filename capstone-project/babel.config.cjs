a19dbe202d25d6037f140f4cfefc0627e8db8690
Amina880 <dkalekwa37@gmail.com>
2024-09-30 14:35:20 +0300

    chore:set up testing

---
+++ capstone-project/babel.config.cjs
@@ -0,0 +1,12 @@
+//set up testing
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+  ],
+};
---
+++ capstone-project/jest.config.js
@@ -0,0 +1,7 @@
+// Set up testing environment
+
+const jestConfig = {
+  testEnvironment: "jsdom",
+};
+
+export default jestConfig;
