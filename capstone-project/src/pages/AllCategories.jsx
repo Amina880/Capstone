bcf2932391fae93c74d7e7b1ef5aedb16478f517
Amina880 <dkalekwa37@gmail.com>
2024-10-01 21:48:57 +0300

    chore:create all recipe categories page

---
+++ capstone-project/src/pages/AllCategories.jsx
@@ -0,0 +1,13 @@
+import Categories from "../components/Categories"
+import Nav from "../components/Nav"
+
+function AllCategories() {
+  return (
+    <>
+    <Nav />
+    <Categories />
+    </>
+  )
+}
+
+export default AllCategories
