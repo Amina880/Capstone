f40c56bdcf76d7c9571c5e2155cc7cd8315a4b33
Amina880 <dkalekwa37@gmail.com>
2024-10-03 16:31:27 +0300

    Chore: edit route

--- capstone-project/src/App.jsx
+++ capstone-project/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home"
 import AllCategories from "./pages/AllCategories"
 import SearchResults from "./pages/SearchResults"
 import RecipeDetailsPage from "./pages/RecipeDetailsPage"
-import FilterUsingCategories from "./components/FilterByCategories"
+import CategoryPage from "./pages/CategoryPage"
 function App() {
   
   return (
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/categories" element={<AllCategories />}/>
-        <Route path="/categories/:categoryName" element={<FilterUsingCategories />}/>
+        <Route path="/categories/:categoryName" element={<CategoryPage />}/>
         <Route path="/results" element={<SearchResults />}/> {/* Search queries are unknown to us  */}
         <Route path="/recipe-details/:recipeId" element={<RecipeDetailsPage />}/> {/*Recipe id is known to us */}
       </Routes>
