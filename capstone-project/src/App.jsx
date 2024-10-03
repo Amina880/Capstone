e289039399b34b2c45b31afee572a0bf4bd413d4
Amina880 <dkalekwa37@gmail.com>
2024-10-03 16:27:51 +0300

    chore:added routes to recipe details and specific category lists

--- capstone-project/src/App.jsx
+++ capstone-project/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import AllCategories from "./pages/AllCategories"
 import SearchResults from "./pages/SearchResults"
+import RecipeDetailsPage from "./pages/RecipeDetailsPage"
+import FilterUsingCategories from "./components/FilterByCategories"
 function App() {
   
   return (
@@ -11,7 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/categories" element={<AllCategories />}/>
-        <Route path="/results" element={<SearchResults />}/>
+        <Route path="/categories/:categoryName" element={<FilterUsingCategories />}/>
+        <Route path="/results" element={<SearchResults />}/> {/* Search queries are unknown to us  */}
+        <Route path="/recipe-details/:recipeId" element={<RecipeDetailsPage />}/> {/*Recipe id is known to us */}
       </Routes>
     </Router>
     </>
