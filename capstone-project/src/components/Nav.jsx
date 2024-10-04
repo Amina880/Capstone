3c8c72168cb191f54e5b565c3c62f4b552dd6cb3
Amina880 <dkalekwa37@gmail.com>
2024-10-04 19:23:23 +0300

    chore: added a link to favourites page

--- capstone-project/src/components/Nav.jsx
+++ capstone-project/src/components/Nav.jsx
@@ -6,6 +6,7 @@ function Nav() {
     <nav>
         <Link to="/">Home</Link>
         <Link to="/categories">Categories</Link>
+        <Link to="/favourites">Favorites</Link>
       
     </nav>
   )
