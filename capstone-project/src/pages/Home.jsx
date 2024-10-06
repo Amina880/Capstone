87a0506f7cb22319fafca0d06f9a61c1e3bad105
Amina880 <dkalekwa37@gmail.com>
2024-10-06 22:38:22 +0300

    styles: made width responsive

--- capstone-project/src/pages/Home.jsx
+++ capstone-project/src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     {/*Home Page */}
       
       <Nav />
-      <div className="mx-auto w-96 md:w-[970px] pt-28 ">
+      <div className="mx-auto max-[390px]:w-72 w-96 md:w-[970px] pt-28 ">
         <div className="pb-20">
             <p className="text-4xl text-center pb-12 font-extralight ">What Would You Like To Eat Today?</p>
             <SearchBar />
