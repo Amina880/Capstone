e33429a839dda9e4475dd4ff7a633a3e11db706a
Amina880 <dkalekwa37@gmail.com>
2024-10-06 20:47:06 +0300

    feat:created the recipe details page

---
+++ capstone-project/src/pages/RecipeDetailsPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import RecipeDetails from '../components/RecipeDetails'
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
+
+function RecipeDetailsPage() {
+  return (
+    <>
+    {/*Recipe Details page */}
+    <Nav />
+    <RecipeDetails />
+    <Footer />
+    </>
+  )
+}
+
+export default RecipeDetailsPage
