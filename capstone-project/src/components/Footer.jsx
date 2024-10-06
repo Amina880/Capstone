47bdd33cda27184e9c521235e322f8d1c7ddec69
Amina880 <dkalekwa37@gmail.com>
2024-10-06 21:15:31 +0300

    chore: creatted footer component

---
+++ capstone-project/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+
+  return (
+    <>
+    <footer className="bg-gray-100 w-full h-auto" >
+        <p className="pt-6 pl-12 text-base font-light" > &copy; 2024 Taste Quest -  All rights reserved</p>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
