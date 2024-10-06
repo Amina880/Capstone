164e7fe815a4d206eeb0288aa1131c86172e6325
Amina880 <dkalekwa37@gmail.com>
2024-10-06 23:23:32 +0300

    Styles: styled the favourites component

--- capstone-project/src/components/FavouritesButton.jsx
+++ capstone-project/src/components/FavouritesButton.jsx
@@ -14,15 +14,19 @@ function FavouritesButton() {
   //Adds the new recipeId to the favRecipes array if it's not already there
   const onClick = () => {
     if(!faveRecipes.includes(recipeId)){
-    const updatedFaves = [...faveRecipes, recipeId];
-    setFaveRecipes(updatedFaves);
-    localStorage.setItem('favRecipes', JSON.stringify(updatedFaves))
+      //Updates the faveRecipes array
+      const updatedFaves = [...faveRecipes, recipeId];
+      setFaveRecipes(updatedFaves);
+      localStorage.setItem('favRecipes', JSON.stringify(updatedFaves))
     }
   }
 
 return (
   <div>
-      <button type="submit" onClick={onClick}>Fave</button>
+    {/*Favourite button */}
+      <button type="submit" onClick={onClick}>
+        <img className="w-8 p-1 mx-4 bg-[#2EC4B6] bg-opacity-65 rounded-2xl" src="/public/Images/3643770_favorite_heart_like_likes_love_icon.png" />
+      </button>
   </div>
 )
 }
