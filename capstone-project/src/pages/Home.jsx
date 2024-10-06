0b6cfdbfeac7d38bb0a0eda53d87f3c5e5dbaeb3
Amina880 <dkalekwa37@gmail.com>
2024-10-06 20:45:41 +0300

    feat:created and styled the home page

---
+++ capstone-project/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import Categories from "../components/Categories";
+import Featured from "../components/FeaturedRecipe";
+import Nav from "../components/Nav";
+import SearchBar from "../components/SearchBar";
+import Footer from "../components/Footer";
+function Home() {
+    
+  return (
+    <>
+    {/*Home Page */}
+      
+      <Nav />
+      <div className="mx-auto w-96 md:w-[970px] pt-28 ">
+        <div className="pb-20">
+            <p className="text-4xl text-center pb-12 font-extralight ">What Would You Like To Eat Today?</p>
+            <SearchBar />
+        </div>
+      
+        <div className="pb-20">
+            <p className="font-semibold text-xl pb-2">Featured Recipe</p>
+            <Featured />
+        </div>
+        <p className="font-semibold md:text-xl md:pb-4 text-lg pb-8 ">Browse Categories</p>
+        <Categories />
+      </div>
+      <Footer />
+       
+    </>
+  )
+}
+
+export default Home
+
