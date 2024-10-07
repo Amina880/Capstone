5a4d8c1e10ef6ad7d61686cddf97024f01a7cbee
Amina880 <dkalekwa37@gmail.com>
2024-10-07 05:57:18 +0300

    styles: changed padding l

--- capstone-project/src/components/Footer.jsx
+++ capstone-project/src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <>
     <footer className="bg-gray-100 w-full h-auto" >
-        <p className="pt-6 pl-12 text-base font-light" > &copy; 2024 Taste Quest -  All rights reserved</p>
+        <p className="pt-6 pl-12 pb-8 text-base font-light" > &copy; 2024 Taste Quest -  All rights reserved</p>
     </footer>
     </>
   )
